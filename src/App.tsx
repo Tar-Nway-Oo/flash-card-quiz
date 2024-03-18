a6b56ca1a466096b019b9492f3e938e3b3023d7f
Tar-Nway-Oo <tarnwayo@gmail.com>
2024-03-18 10:30:51 +0630

    complete flashCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import Form from "./components/Form"
 import FlashCard from "./components/FlashCard"
 import "./index.css"
+import { useEffect, useState } from "react"
+
+export type cards = {
+ id: number
+ question: string
+ options: string[]
+ answer: string
+}
 
 export default function App() {
 
-  const cards = [
-    {id: 1, q: "what's 2+2?", options: ["2", "4", "6"]},
-    {id: 1, q: "what's 2+2?", options: ["2", "4", "6"]},
-    {id: 1, q: "what's 2+2?", options: ["2", "4", "6"]},
-    {id: 1, q: "what's 2+2?", options: ["2", "4", "6"]},
-    {id: 1, q: "what", options: ["2", "4", "6"]}
-  ]
+  const [cards, setCards] = useState<cards[]>([]);
+
+  useEffect(() => {
+    fetch(`https://opentdb.com/api.php?amount=10`)
+    .then(res => res.json())
+    .then(data => {
+      setCards(data.results.map((result: any, index: number) => {
+        const answer = decodeString(result.correct_answer);
+        const options = [...result.incorrect_answers.map((answer: string) => decodeString(answer)), answer];
+        return {
+        id: `${index}-${Date.now()}`,
+        question: decodeString(result.question),
+        answer: answer,
+        options: options.sort(() => Math.random() - .5)
+        }
+      }));
+    });
+  }, [])
+
+  function decodeString(str: string) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
 
   return (
     <div>
--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -1,19 +1,25 @@
+import { useState } from "react"
+import { cards } from "../App";
 import "../index.css"
 
-type cards = {
-   id: number
-   q: string
-   options: string[]
-}
-export default function FlashCard({id, q, options}: cards) {
+
+export default function FlashCard({answer, question, options}: cards) {
+
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
-    <div className="card">
-      <p>{q}</p>
-      <ul>
-         {options.map(o => (
-            <li>{o}</li>
-         ))}
-      </ul>
+    <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={() => setIsFlipped(prev => !prev)}>
+        <div className="front-side">
+          <p className="question">{question}</p>
+          <ul className="options">
+            {options.map(option => (
+              <li key={option}>{option}</li>
+             ))}
+          </ul>
+        </div>
+        <div className="back-side">
+          <p className="answer">{answer}</p>
+        </div>
     </div>
   )
 }
